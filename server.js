87b331e0c1d323c4cb2626ea1b03ea782611a6eb
James Ocana <jamesocana@macbook-air.myfiosgateway.com>
2022-09-08 13:52:10 -0400

    mongoDB connected

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 require("dotenv").config()
 const PORT = process.env.PORT;
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.listen(PORT, (req, res) => {
     console.log("Server is connected on port " + PORT);
